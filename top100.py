3126adec112a89989ad1ea2a4c9dea598e9e87d2
Folgert Karsdorp <fkarsdorp@fancypants.science.ru.nl>
2013-10-02 13:47:30 +0200

    update

--- top100.py
+++ top100.py
@@ -7,6 +7,7 @@ from itertools import combinations
 
 import numpy as np
 import pandas as pd
+import networkx as nx
 
 from joblib import Parallel, delayed
 
@@ -27,12 +28,13 @@ def count_persons(decade):
 
 if __name__ == '__main__':
     root = sys.argv[1]
+    n = int(sys.argv[2])
     counters = Parallel(n_jobs=-1, verbose=5)(
         delayed(count_persons)(
             os.path.join(root, decade)) for decade in os.listdir(root))
     counts = sum(counters, Counter())
-    top100 = dict((name, i) for i, name in enumerate(nlargest(50, counts, key=counts.__getitem__)))
-    cooccurrences = np.zeros((50, 50))
+    top_n = dict((name, i) for i, name in enumerate(nlargest(n, counts, key=counts.__getitem__)))
+    cooccurrences = np.zeros((n, n))
     for decade in os.listdir(root):
         print decade
         decade = os.path.join(root, decade)
@@ -45,13 +47,18 @@ if __name__ == '__main__':
                     if named_entity == 'PERSON':
                         previous_person += ' ' + word
                     elif previous_person:
-                        if previous_person.lower() in top100:
+                        if previous_person.lower() in top_n:
                             persons.add(previous_person.lower())
                         previous_person = ''
             for person_a, person_b in combinations(persons, 2):
-                cooccurrences[top100[person_a], top100[person_b]] += 1
-                cooccurrences[top100[person_b], top100[person_a]] += 1
-    names = sorted(top100, key=top100.__getitem__)
+                cooccurrences[top_n[person_a], top_n[person_b]] += 1
+                cooccurrences[top_n[person_b], top_n[person_a]] += 1
+    names = sorted(top_n, key=top_n.__getitem__)
+    G = nx.DiGraph()
+    for i, name_i in enumerate(names):
+        for j, name_j in enumerate(names):
+            if j != i:
+                G.add_edge(name_i, name_j, weight=cooccurrences[i, j])
     df = pd.DataFrame(cooccurrences, columns=names, index=names)
-    df.to_csv("top100.csv")
+    df.to_csv("top-%s.csv" % n)
 
